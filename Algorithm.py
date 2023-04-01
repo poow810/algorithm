cfa3e0e184a529e52faae8f58c5c8dbe75ceb71c
poow810 <poow810@naver.com>
2023-04-01 23:57:06 +0900

    메롱

--- Algorithm.py
+++ Algorithm.py
@@ -1,31 +1,20 @@
-# 인접 리스트로 DFS 구현
-n = 5
-graph = [[] for _ in range(n+1)]
+from collections import deque
 
-graph[1] = [2, 3]
-graph[2] = []
-graph[3] = [4, 5]
-graph[4] = []
-graph[5] = []
 
-visited = [False]*(n+1)
-def dfs(graph, idx, visited):
-    stack = [idx]   # 노드를 스택에 넣음
-    visited[idx] = True   # 방문한 노드
+def bfs(matrix, idx, visited):
+    queue = deque()
+    queue.append(idx)
 
-    while stack:    # 스택이 빌 때까지 반복
-        current = stack.pop()  # 현재 위치한 노드
+    while queue:
+        current = queue.popleft()
 
         if not visited[current]:
             print(current)
             visited[current] = True
 
-        for child in graph[current]:
-            if not visited[child]:
-                stack.append(child)
+        for child in range(len(matrix[current])):
+            if matrix[current][child] == 1 and visited[child]:
+                queue.append(child)
 
-dfs(graph, 1, visited)
-
-
-
+bfs(matrix, 1, visited)
 
